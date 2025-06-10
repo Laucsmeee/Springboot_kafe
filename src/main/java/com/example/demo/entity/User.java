bb0b616ac8b094332b63360e2d1347a8113bc420
Laucsmeee <Laucsmee@gmail.com>
2025-06-11 00:21:00 +0300

    dockerfile

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -16,20 +16,59 @@ public class User {
     private String qrCode;
     private int bonusPoints;
 
+    // id - геттер і сеттер (якщо треба)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // username
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
+    // password
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
+    // birthdate
+    public String getBirthdate() {
+        return birthdate;
+    }
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    // phone
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // qrCode
+    public String getQrCode() {
+        return qrCode;
+    }
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
+
+    // bonusPoints
+    public int getBonusPoints() {
+        return bonusPoints;
+    }
+    public void setBonusPoints(int bonusPoints) {
+        this.bonusPoints = bonusPoints;
+    }
 }
